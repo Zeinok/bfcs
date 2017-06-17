67235c71c961864ac9f60ec960853cf8c4733bc3
Senoc <da.ze@msa.hinet.net>
2017-06-17 20:49:27 +0800

    init

---
+++ bfcs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace bfcs {
+    
+    class Program {
+        static char[] cells = new char[Int16.MaxValue];
+        static int cellLocation = 0;
+        static void Main(string[] args) {
+            string bfString;
+            if(args.Length == 0){
+                //print usage
+                string[] usage = {"brainfuck C# Usage:",
+                                  "    bfcs.exe <brainfuck string>",
+                                  "    bfcs.exe -f <file>"
+                                 };
+                foreach(string line in usage) {
+                    Console.WriteLine(line);
+                }
+            }else if(args[0] == "-f") {
+                //from file
+                try {
+                    bfString = System.IO.File.ReadAllText(args[1]);
+                    bfcs.bfParse(bfString.ToCharArray(), false);
+                } catch(Exception e) {
+                    Console.WriteLine(e.Message);
+                }
+            } else {
+                bfString = String.Join("", args);
+                bfcs.bfParse(bfString.ToCharArray(), false);
+            }
+        }
+       
+    }
+}
---
+++ bfcs/bfcs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bfcs {
+    class bfcs {
+        private static char[] cells = new char[Int16.MaxValue];
+        private static int cellLocation = 0;
+        public static int bfParse(char[] commands, bool isLoop) {
+        redo:
+            //Console.Write(String.Join("",commands));
+            if(isLoop & (int)cells[cellLocation] == 0) return 0;
+            for(long i = 0;i < commands.LongLength;++i) {
+                switch(commands[i]) {
+                    case '<':
+                        --cellLocation;
+                        break;
+                    case '>':
+                        ++cellLocation;
+                        break;
+                    case '+':
+                        ++cells[cellLocation];
+                        break;
+                    case '-':
+                        --cells[cellLocation];
+                        break;
+                    case '.':
+                        Console.Write(cells[cellLocation]);
+                        break;
+                    case '!':
+                        Console.Write((int)cells[cellLocation] + " ");
+                        break;
+                    case ',':
+                        cells[cellLocation] = Console.ReadKey().KeyChar;
+                        break;
+                    case '[':
+                        ulong passedLeftSquareBracket = 0;
+                        string loopString = "";
+                        for(;;) {
+                            ++i;
+                            if(commands[i] == '[') ++passedLeftSquareBracket;
+                            if(commands[i] == ']') {
+                                if(passedLeftSquareBracket > 0) {
+                                    --passedLeftSquareBracket;
+                                } else {
+                                    break;
+                                }
+                            }
+                            loopString += commands[i].ToString();
+                        }
+                        bfParse(loopString.ToCharArray(), true);
+                        break;
+                }
+            }
+            if(isLoop) goto redo;
+            return 0;
+
+
+
+
+        }
+    }
+}
